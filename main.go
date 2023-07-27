3d72becdfcceb2f3c3b40483a03f7830700df745
Tejas Dixt <tejasdixit17@gmail.com>
2023-07-27 13:08:16 -0400

    added another command

--- main.go
+++ main.go
@@ -10,21 +10,29 @@ func main() {
 	var rootCmd = &cobra.Command{Use: "cli"}
 	//Executing the root command
 
-	//Hello command 
+	//Hello command
 	var helloCmd = &cobra.Command{
-		Use: "hello",
+		Use:   "hello",
 		Short: "Print simple greeting on screen",
-		Long: ``,
-		Run : func(cmd *cobra.Command, args [] string){
+		Long:  ``,
+		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Hello from PAANDATD ")
 		},
 	}
-
-	rootCmd.AddCommand(helloCmd)
-	
+	//about commaand
+	var aboutCmd = &cobra.Command{
+		Use:   "about",
+		Short: "PANDATD's cli application for daily use",
+		Run: func(cmd *cobra.Command, args []string) {
+			var message string
+			message = "Hello There my name is Tejas Dixit"
+			fmt.Println(message)
+		},
+	}
+	rootCmd.AddCommand(helloCmd, aboutCmd)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
+
 	}
-	
 
 }
